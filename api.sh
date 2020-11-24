65237a57a52670008661387c72fd4fa5dc2213db
Itsbeeanlongday <dbtjdals1771@ajou.ac.kr>
2020-11-24 21:50:29 +0900

    add result

--- api.sh
+++ api.sh
@@ -1,11 +1,9 @@
 curl --request GET \
-  --url 'https://web-production.lime.bike/api/rider/v1/login?phone=%2B821036231771'
+  --url 'https://web-production.lime.bike/api/rider/v1/login?phone=%2B821087284736'
 
 read -p "enter login code >> " code
 
-login_params="{\"login_code\":\"$code\",\"phone\":\"+821036231771\"}"
-
-echo $login_params
+login_params="{\"login_code\":\"$code\",\"phone\":\"+821087284736\"}"
 
 curl --request POST \
   --cookie-jar - \
@@ -14,10 +12,7 @@ curl --request POST \
   --data $login_params
 
 read -p "enter token >> " token
-read -p "enter cookie >> " cookie
 
 curl --request GET \
   --url 'https://web-production.lime.bike/api/rider/v1/views/map?ne_lat=52.6&ne_lng=13.5&sw_lat=52.4&sw_lng=13.3&user_latitude=52.5311&user_longitude=13.3849&zoom=16' \
-  --header 'authorization: Bearer '$token \
-  --cookie '_limebike-web_session='$cookie 
-
+  --header 'authorization: Bearer '$token
